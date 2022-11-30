63430f50c699ce449d53f89caee38181d53234fe
kiyong <dky0979@gmail.com>
2022-11-30 10:58:32 +0900

    [DependencyInjection] 프로젝트 세팅

---
+++ Swift_ToyProject/Swift_ToyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_ToyProject
+//
+//  Created by Do Kiyong on 2022/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
