65c1190dc4388b4fb1406ea0a6088c9c877ab188
kiyong <dky0979@gmail.com>
2022-12-07 12:46:49 +0900

    [FileDownloader_Preview] 세팅

---
+++ Swift_ToyProject/Study/FileDownloader_Preview/FileDownloader_Preview/Utils.swift
@@ -0,0 +1,49 @@
+//
+//  Utils.swift
+//  FileDownloader_Preview
+//
+//  Created by Do Kiyong on 2022/12/07.
+//
+
+import Foundation
+
+class Utils {
+    static func downloadFileAsync(url: URL, completion: @escaping (String?, Error?) -> Void) {
+        let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let destinationUrl = documentsUrl.appendingPathComponent(url.lastPathComponent)
+        
+        if FileManager().fileExists(atPath: destinationUrl.path) {
+            print("File already exists [\(destinationUrl.path)]")
+            
+            completion(destinationUrl.path, nil)
+        } else {
+            let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: nil)
+            var request = URLRequest(url: url)
+            
+            request.httpMethod = "GET"
+            
+            let task = session.dataTask(with: request, completionHandler: { data, response, error in
+                if error == nil {
+                    if let response = response as? HTTPURLResponse {
+                        if response.statusCode == 200 {
+                            if let data = data {
+                                if let _ = try? data.write(to: destinationUrl, options: Data.WritingOptions.atomic) {
+                                    completion(destinationUrl.path, error)
+                                } else {
+                                    completion(destinationUrl.path, error)
+                                }
+                            } else {
+                                completion(destinationUrl.path, error)
+                            }
+                        }
+                    }
+                } else {
+                    completion(destinationUrl.path, error)
+                }
+            })
+            
+            task.resume()
+        }
+    }
+}
