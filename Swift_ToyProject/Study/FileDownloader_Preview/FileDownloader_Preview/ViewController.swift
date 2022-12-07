b35266c7cefd578cf42154ae56f11587d3a374e5
kiyong <dky0979@gmail.com>
2022-12-07 13:54:11 +0900

    [FileDownloader_Preview] 완료

--- Swift_ToyProject/Study/FileDownloader_Preview/FileDownloader_Preview/ViewController.swift
+++ Swift_ToyProject/Study/FileDownloader_Preview/FileDownloader_Preview/ViewController.swift
@@ -9,104 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // Sample PDF URL
-    let pdfURL = "http://www.africau.edu/images/default/sample.pdf"
-    
     // UIDocumentInteractionController
     var interaction:UIDocumentInteractionController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
         
     }
     
     @IBAction func startFileDownload() {
-        fileDownload { (success, path) in
+        guard let url = URL(string: "https://www.tutorialspoint.com/swift/swift_tutorial.pdf") else { return }
+        
+        Utils.downloadFileAsync(url: url, completion: { (path, error) in
             DispatchQueue.main.async(execute: {
+                guard let path = URL(string: path ?? "") else { return }
                 self.openFileWithPath(pdfPath: path)
             })
-        }
-    }
-    
-    func fileDownload(completion: @escaping (_ success:Bool, _ pdfURL:URL) -> ()) {
-        
-        // Check pdfURL
-        guard let url = URL(string: pdfURL) else {
-            print("Error: cannot create URL")
-            return
-        }
-        
-        // set up URLRequest
-        let urlRequest = URLRequest(url: url)
-        
-        // set up the session
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        
-        
-        let task = session.dataTask(with: urlRequest) { (data, response, error) in
-            
-            // getting Data Error
-            guard error == nil else {
-                debugPrint("Error!!")
-                return
-            }
-            
-            // Respose Data Empty
-            guard let responseData = data else {
-                print("Error: data empty!!")
-                return
-            }
-            
-            // FileManager 인스턴스 생성
-            let fileManager = FileManager()
-            
-            // document 디렉토리의 경로 저장
-            let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-            
-            // 해당 디렉토리 이름 지정
-            let dataPath = documentsDirectory.appendingPathComponent("FileManager Directory")
-            
-            do {
-                // 디렉토리 생성
-                try fileManager.createDirectory(atPath: dataPath.path, withIntermediateDirectories: false, attributes: nil)
-                
-            } catch let error as NSError {
-                print("Error creating directory: \(error.localizedDescription)")
-            }
-            
-            do {
-                
-                // 파일 이름을 기존의 경로에 추가
-                let writePath = dataPath.appendingPathComponent("sample.pdf")
-                
-                // 쓰기 작업
-                try responseData.write(to: writePath)
-                
-                completion(true, writePath)
-                
-            } catch let error as NSError {
-                print("Error Writing File : \(error.localizedDescription)")
-            }
-            
-        }
-        
-        task.resume()
-        
+        })
     }
     
     func openFileWithPath(pdfPath : URL) {
         interaction = UIDocumentInteractionController(url: pdfPath)
         interaction?.delegate = self
-        interaction?.presentPreview(animated: true) // IF SHOW DIRECT
+        interaction?.presentPreview(animated: true)
     }
-    
     
 }
 
 extension ViewController : UIDocumentInteractionControllerDelegate {
     public func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        
         return self
     }
     
