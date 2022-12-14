0a4812ebc1d646a065962a2763b7713d1e66d0c1
kiyong <dky0979@gmail.com>
2022-12-14 11:41:59 +0900

    [Lazyvariables] 완료

--- Swift_ToyProject/Study/LazyVariables/LazyVariables/ViewController.swift
+++ Swift_ToyProject/Study/LazyVariables/LazyVariables/ViewController.swift
@@ -9,55 +9,192 @@
 
 import UIKit
 
-// [6단계]
-// [Common Scenarios for Retain Cycles : Closures]
-// Closure는 Retain Cycle이 빈번히 일어나는 시나리오 중 하나이다.
+// [8단계]
+// [Lazy variables]
+// lazy 변수는 처음 사용되기 전까지는 연산이 되지 않는다는 것이다.
+
+// 1. lazy는 반드시 var와 함께 쓰여야 한다.
+// -> 기본적으로 lazy로 선언된 변수는 초기에는 값이 존재하지 않고 이후에 값이 생기는 것이기 때문에 let 으로는 선언될 수 없다.
+
+// 2. struct, class
+// -> 기본적으로 lazy는 struct와 class에서만 사용할 수 있다.
+
+// 3. lazy vs Computed Property
+// -> Computed Property에는 lazy 키워드를 사용할 수 없다.
+//    lazy는 처음 사용될 때 메모리에 값을 올리고 그 이후 부터는 계속해서 메모리에 올라온 값을 사용한다.
+//    사용할 때마다 값을 연산하여 사용하는 Computed Property에는 사용할 수 없다.
+
+// 4. lazy and closure
+// -> lazy에 어떤 특별한 연산을 통해 값을 넣어주기 위해서는 코드 실행 블록인 closure를 사용한다.
+//    class나 struct의 다른 프로퍼티의 값을 lazy 변수에서 사용하기 위해서는 closure내에서 self를 통해 접근이 가능하다.
+//    기본적으로 일반 변수들은 클래스가 생성된 이후에 접근이 가능하기 때문에 클래스내의 다른 영역(메소드, 일반 프로퍼티)에서는 self를 통해 접근할 수 없지만
+//    lazy키워드가 붙으면 생성 후 추후에 접근할 것이라는 의미이기 때문에 closure내에서 self로 접근이 가능하다.
 
 class ViewController: UIViewController {
-
-    var testClass: TestClass? = TestClass()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        testClass = nil
+        var me = Person(name:"John")
 
-        // TestClass의 객체의 내부에서 Closure로, Closure에서 TestClass객체로 강한 참조를 하고 있기 때문에
-        // TestClass 객체의 메모리가 해제되지 않는다.
-        
-        // [출력]
-        // init
-        
-        // Closure 역시 Class와 마찬가지로 Reference Type 이기 때문에
-        // ✅[weak self]✅를 capture해줌으로써 해결할 수 있다.
-        // 이렇게 되면 TestClass Instance는 Closure를 강한참조, Closure는 TestClass Instance를 약한참조 하게된다.
-        
-        // [출력]
-        // init
-        // deinit
+//        print(me.greeting)
+        print(me.greeting())
+
+        me.name = "James"
+
+//        print(me.greeting)
+        print(me.greeting())
+
     }
 
 }
 
-class TestClass {
+class Person {
+    var name: String
     
-    var aBlock: (()->())? = nil
-
-    let aConstant = 5
+    // self.name을 통해 name프로퍼티에 접근하는 것을 볼 수 있는데
+    // 아래의 단계들에서 클래스 내부의 클로저에서 클래스 객체를 self 로 참조한다면 메모리 누수가 발생하는 위험이 있다는 것을 알았다.
+    // 하지만 뒤의 ()를 통해 그 즉시 실행하고 결과를 돌려주고 끝나버리기 때문에 메모리 누수의 걱정은 없다.
+//    lazy var greeting: String = {
+//        // 프로퍼티가 James으로 변경이 되어도 처음 사용할 때 메모리에는 John이 올라가 있기 때문에 John이 출력이 된다.
+//        return "Hello my name is \((self.name))"
+//
+//        // [출력]
+//        // Hello my name is John
+//        // Hello my name is John
+//    }() // ✅
+    
+    lazy var greeting: () -> String = { [weak self] in
+        // 만일 변수가 lazy var greeting:String이 아닌 lazy var greeting: () -> String으로
+        // 클로저 실행의 결과를 담는 것이 아닌 클로저 자체를 담고 있는 변수라면 반드시 [weak self]를 통해 메모리 누수를 방지해주어야 한다.
         
-        init() {
-            print("init")
-            
-            aBlock = { [weak self] in // ✅ [weak self]를 추가하여
-                print(self?.aConstant)
-            }
-        }
-    
-        deinit {
-            print("deinit")
-        }
-
+        // 값이 아닌 클로저 자체가 메모리에 올라가 있고, self 는 내부에서 계속해서 클래스를 참조하기 때문에
+        // 계속 John이 출력이 되는 것이 아닌 James가 출력이 되는 것이다.
+        return "Hello my name is \(((self?.name))!)"
+        
+        // [출력]
+        // Hello my name is John
+        // Hello my name is James
+    }
+  
+    init(name: String){
+        self.name = name
+    }
 }
+
+
+//// [7단계]
+//// [Detecting retain cycles by using log messages in deinit]
+//// 순환참조를 감지하는 방법은 deinit에 로그메시지를 출력하는 코드를 넣는 것이다.
+//// 이는 매우 효과적인 방법이다.
+//
+//// View Controller마다 이러한 메소드를 작성해주는 것이 좋다.
+//// 예를 들어 View Controller를 pop한다면 Log 메시지는 출력이 되어야 한다.
+//// 만일 출력이 된다면 프로그램이 잘 돌아가고있음을 알 수 있다.
+//
+//class ViewController: UIViewController {
+//
+//    var testClass: TestClass? = TestClass()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        testClass = nil
+//
+//    }
+//
+//}
+//
+//class TestClass {
+//
+//    init() {
+//        print("init")
+//    }
+//
+//    deinit {
+//        print("deinit")
+//    }
+//
+//}
+
+
+//// [6단계]
+//// [Common Scenarios for Retain Cycles : Closures]
+//// Closure는 Retain Cycle이 빈번히 일어나는 시나리오 중 하나이다.
+//
+//class ViewController: UIViewController {
+//
+//    var testClass: TestClass? = TestClass()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+////        testClass = nil
+//
+//        // TestClass의 객체의 내부에서 Closure로, Closure에서 TestClass객체로 강한 참조를 하고 있기 때문에
+//        // TestClass 객체의 메모리가 해제되지 않는다.
+//
+//        // [출력]
+//        // init
+//
+//        // Closure 역시 Class와 마찬가지로 Reference Type 이기 때문에
+//        // ✅[weak self]✅를 capture해줌으로써 해결할 수 있다.
+//        // 이렇게 되면 TestClass Instance는 Closure를 강한참조, Closure는 TestClass Instance를 약한참조 하게된다.
+//
+//        // [출력]
+//        // init
+//        // deinit
+//
+//        testClass?.doSomething()
+//        testClass = nil
+//
+//        // [출력]
+//        // init
+//        // deinit
+//    }
+//
+//}
+//
+//class TestClass {
+//
+//    var aBlock: (()->())? = nil
+//
+//    let aConstant = 5
+//
+//    init() {
+//        print("init")
+//
+////        aBlock = { [weak self] in // ✅ [weak self]를 추가하여 약한참조를 걸어준다.
+////            // 그러나 Closure를 사용한다고 해서 항상 순환참조가 발생하는것은 아니다.
+////            print(self?.aConstant)
+////        }
+//
+////        let aBlock = { // ✅ [weak self]를 추가할 필요가 없다.
+////            // Closure 블록을 locally하게만 사용한다면 self를 weak하게 capture할 필요가 없다.
+////            // 그 이유는 Closure 블록에 대한 강한참조가 존재하지 ㅇ낳기 때문이다.
+////
+////            // 블록 자체는 블록 내부에서 self, 즉 TestClass 객체를 강하게 참조하지만
+////            // Closure자체는 메소드 지역안에 존재하기 때문에 메소드가 return되면 메모리에서 해제됩니다.
+////            print(self.aConstant)
+////        }
+//    }
+//
+//    deinit {
+//        print("deinit")
+//    }
+//
+//    func doSomething(){
+//        UIView.animate(withDuration: 5) {
+//            let aConstant = self.aConstant
+//
+//            // fancy animation ... .
+//
+//            // Closure블록에 대한 강한 참조가 존재하지 않는다면 Retain Cycle에 대해 걱정할 필요가 없다.
+//            // 또한, unowned 역시 weak대신 사용할 수 있고 이전에 사용했던 예제는 unowned보다 안전하게 사용이 가능하다.
+//        }
+//    }
+//
+//}
 
 
 //// [5단계]
