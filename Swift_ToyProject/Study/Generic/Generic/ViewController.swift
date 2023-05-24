291286467e199d32bbd7773cc7a410f9c849998d
kiyong <dky0979@gmail.com>
2023-05-24 16:18:02 +0900

    no message

--- Swift_ToyProject/Study/Generic/Generic/ViewController.swift
+++ Swift_ToyProject/Study/Generic/Generic/ViewController.swift
@@ -113,41 +113,4 @@ extension Stack {
     }
 }
 
-// ✅ 연관 타입 (Associated Types)
-protocol Container {
-    associatedtype Item
-    
-    mutating func append(_ item: Item)
-    
-    var count: Int { get }
-    
-    subscript(i: Int) -> Item { get }
-}
 
-struct IntStack: Container {
-    // original IntStack implementation
-    var items = [Int]()
-    
-    mutating func push(_ item: Int) {
-        items.append(item)
-    }
-    
-    mutating func pop() -> Int {
-        return items.removeLast()
-    }
-    
-    // conformance to the Container protocol
-    typealias Item = Int
-    
-    mutating func append(_ item: Int) {
-        self.push(item)
-    }
-    
-    var count: Int {
-        return items.count
-    }
-    
-    subscript(i: Int) -> Int {
-        return items[i]
-    }
-}
